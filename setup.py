bebe5e2f94400b36f639fd6db8797274dd489a9f
Greg Guthe <gguthe@mozilla.com>
2017-10-04 17:19:16 -0400

    Add cookiecutter template

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import setup
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='pytest-aws',
+    version='0.1.0',
+    author='Mozilla Services',
+    author_email='services-dev@mozilla.com',
+    maintainer='Mozilla Services',
+    maintainer_email='services-dev@mozilla.com',
+    license='Mozilla Public License 2.0',
+    url='https://github.com/mozilla-services/pytest-aws',
+    description='pytest plugin for testing AWS resource configurations',
+    long_description=read('README.md'),
+    py_modules=['pytest_aws'],
+    install_requires=['pytest>=3.1.1'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Operating System :: OS Independent',
+    ],
+    entry_points={
+        'pytest11': [
+            'aws = pytest_aws',
+        ],
+    },
+)
