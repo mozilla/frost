61514d2fe316ca4b8fadaa259056df13c0e0ab20
Greg Guthe <gguthe@mozilla.com>
2017-10-10 11:56:36 -0400

    add test file from template

---
+++ tests/test_fixtures.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+
+def test_help_message(testdir):
+    result = testdir.runpytest(
+        '--help',
+    )
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines([
+        'aws:',
+        '*--foo=DEST_FOO*Set the value for the fixture "bar".',
+    ])
+
+
+def test_hello_ini_setting(testdir):
+    testdir.makeini("""
+        [pytest]
+        HELLO = world
+    """)
+
+    testdir.makepyfile("""
+        import pytest
+
+        @pytest.fixture
+        def hello(request):
+            return request.config.getini('HELLO')
+
+        def test_hello_world(hello):
+            assert hello == 'world'
+    """)
+
+    result = testdir.runpytest('-v')
+
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines([
+        '*::test_hello_world PASSED',
+    ])
+
+    # make sure that that we get a '0' exit code for the testsuite
+    assert result.ret == 0
