db1285c3b3f837f0a094716f4524cf1d14da55fc
Greg Guthe <gguthe@mozilla.com>
2018-01-15 17:32:20 +0700

    remove old pytest

--- rules/rds/rds_db_instance_is_multiaz.py
+++
@@ -1,7 +0,0 @@
-
-import pytest
-
-
-@pytest.mark.rds
-def test_rds_db_instance_is_multiaz(rds_db_instance):
-    assert rds_db_instance['MultiAZ'] == False, '{} is in a single availability zone'.format(rds_db_instance['DBInstanceIdentifier'])
