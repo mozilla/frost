61db312075a4814524b2096964e19fc75a6da1c1
AJ Bahnken <aj@ajvb.me>
2018-01-18 16:30:37 -0800

    added docstring to is_ebs_volume_encrypted

--- aws/ec2/test_ec2_ebs_volume_encrypted.py
+++ aws/ec2/test_ec2_ebs_volume_encrypted.py
@@ -3,6 +3,26 @@ import pytest
 from aws.ec2.resources import ec2_ebs_volumes
 
 def is_ebs_volume_encrypted(ec2_ebs_volume):
+    """
+    Checks the EBS volume 'Encrypted' value.
+
+    >>> is_ebs_volume_encrypted({'Encrypted': True})
+    True
+    >>> is_ebs_volume_encrypted({'Encrypted': False})
+    False
+    >>> is_ebs_volume_encrypted({})
+    Traceback (most recent call last):
+    ...
+    KeyError: 'StorageEncrypted'
+    >>> is_ebs_volume_encrypted(0)
+    Traceback (most recent call last):
+    ...
+    TypeError: 'int' object is not subscriptable
+    >>> is_ebs_volume_encrypted(None)
+    Traceback (most recent call last):
+    ...
+    TypeError: 'NoneType' object is not subscriptable
+    """
     return bool(ec2_ebs_volume['Encrypted'])
 
 @pytest.mark.ec2
