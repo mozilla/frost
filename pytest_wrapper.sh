a840241d95adf0cfe39ac790fec2a03b56c3e146
AJ Bahnken <aj@ajvb.me>
2018-01-22 14:34:36 -0800

    have pytest_wrapper work with new make cmd + deployment flow

--- pytest_wrapper.sh
+++ pytest_wrapper.sh
@@ -8,23 +8,25 @@ set -e
 # Wrapper script for running the pytest service tests in Jenkins
 
 # Docker should mount an ~/.aws/credentials file
-# or provide cred env vars http://docs.aws.amazon.com/cli/latest/userguide/cli-environment.html
 
-cd /foxsec/tools/pytest-services
 make install
 
 # allow pytest commands to fail so we can report results
 set +e
-make clean all
+make awsci AWS_PROFILE=$AWS_PROFILE
 set -e
 
-# Check in the generated files
 date=`date +%F`
-cp $date.* /foxsec-results/pytest-services
-cd /foxsec-results/pytest-services
+venv/bin/python3 report_json_to_service_json.py \
+  -i results-${AWS_PROFILE}-${date}.json \
+  -o service-report-${AWS_PROFILE}-${date}.json
 
+# Check in the generated files
+mkdir -p /$RESULTS_DIR/aws-pytest/${AWS_PROFILE}
+cp service-report-${AWS_PROFILE}-${date}.json /$RESULTS_DIR/aws-pytest/${AWS_PROFILE}/
+
+cd /$RESULTS_DIR/
 git pull
-
-git add $date.*
-git commit -m "Pytest Results $date"
+git add aws-pytest/${AWS_PROFILE}/service-report-${AWS_PROFILE}-${date}.json
+git commit -m "Pytest Services Results ${AWS_PROFILE} ${date}"
 git push origin master:master
