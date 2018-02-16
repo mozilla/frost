2d519ad986b2c7354d7114238df8b9c8bba9c185
AJ Bahnken <aj@ajvb.me>
2018-02-16 09:43:21 -0800

    Add MFA on user with console access test (#87)

---
+++ aws/iam/test_iam_user_without_mfa.py
@@ -0,0 +1,17 @@
+import pytest
+
+from aws.iam.resources import (
+    iam_user_login_profiles,
+    iam_user_mfa_devices,
+)
+
+
+@pytest.mark.iam
+@pytest.mark.parametrize(
+        ['iam_login_profile', 'iam_user_mfa_devices'],
+        zip(iam_user_login_profiles(), iam_user_mfa_devices()),
+        ids=lambda login: login['UserName'])
+def test_iam_user_without_mfa(iam_login_profile, iam_user_mfa_devices):
+    if bool(iam_login_profile):
+        assert len(iam_user_mfa_devices) > 0, \
+            'No MFA Device found for {}'.format(iam_login_profile['UserName'])
