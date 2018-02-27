0038e8ed9e46e90ad45ed397c2a245a8fcabf821
Greg Guthe <gguthe@mozilla.com>
2018-02-27 12:22:32 -0500

    fix flake8 errors in iam resource tests

--- aws/iam/meta_test_resources.py
+++ aws/iam/meta_test_resources.py
@@ -14,11 +14,12 @@ def test_iam_users():
             'Path': '/',
             'UserId': 'H0BBIHMA0CZ0R0K0MN00C',
             'UserName': 'tigerone',
-        '__pytest_meta': {'profile': 'example-account', 'region': 'us-east-1'}},
-        {'Arn': 'arn:aws:iam::123456789012:user/calvin',
-             'CreateDate': '1985-11-18T00:01:10+00:00',
-             'PasswordLastUsed': '2008-01-09T20:43:00+00:00',
-             'Path': '/',
-             'UserId': 'CALCIHMA0CZ0R0K0MN00C',
-             'UserName': 'spacemanspiff',
-        '__pytest_meta': {'profile': 'example-account', 'region': 'us-east-1'}}]
+            '__pytest_meta': {'profile': 'example-account', 'region': 'us-east-1'}},
+        {
+            'Arn': 'arn:aws:iam::123456789012:user/calvin',
+            'CreateDate': '1985-11-18T00:01:10+00:00',
+            'PasswordLastUsed': '2008-01-09T20:43:00+00:00',
+            'Path': '/',
+            'UserId': 'CALCIHMA0CZ0R0K0MN00C',
+            'UserName': 'spacemanspiff',
+            '__pytest_meta': {'profile': 'example-account', 'region': 'us-east-1'}}]
