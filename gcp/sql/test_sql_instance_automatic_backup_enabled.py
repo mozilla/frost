dda1df9cc193fc121557ff26a67c99f609ce0d02
AJ Bahnken <aj@ajvb.me>
2018-03-07 16:44:34 -0800

    Added automatic backup enabled test

---
+++ gcp/sql/test_sql_instance_automatic_backup_enabled.py
@@ -0,0 +1,13 @@
+import pytest
+
+from gcp.sql.resources import instances
+
+
+@pytest.mark.gcp_sql
+@pytest.mark.parametrize('sql_instance',
+                         instances(),
+                         ids=lambda instance: instance['name'])
+def test_sql_instance_automatic_backup_enabled(sql_instance):
+    """Test CloudSQL Instance has Automatic Backup Enabled
+    """
+    assert sql_instance.get('settings').get('backupConfiguration').get('binaryLogEnabled')
