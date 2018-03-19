30d3cdc71a848f383951874b23c64b7128bdf2e9
AJ Bahnken <aj@ajvb.me>
2018-03-19 13:17:44 -0700

    Cleaned up compute helpers

--- gcp/compute/helpers.py
+++ gcp/compute/helpers.py
@@ -2,24 +2,31 @@
 
 def does_firewall_open_all_ports_to_all(firewall):
     """
-    TODO
+    Returns True if firewall has a rule to open all ports to all. Excludes ICMP.
+
+    >>> does_firewall_open_all_ports_to_all({})
+    False
+    >>> does_firewall_open_all_ports_to_all({'sourceRanges': ['1.1.1.1/1']})
+    False
+    >>> does_firewall_open_all_ports_to_all({'sourceRanges': ['1.1.1.1/1'], 'allowed': [{'ports': '0-65535'}]})
+    False
+    >>> does_firewall_open_all_ports_to_all({'sourceRanges': ['0.0.0.0/0'], 'allowed': [{'ports': '0-65535'}]})
+    True
     """
     if firewall.get("sourceRanges") is None or "0.0.0.0/0" not in firewall['sourceRanges']:
         return False
 
-    if not firewall.get('allowed', [])[0].get('ports'):
-        if firewall.get('allowed', [])[0].get('IPProtocol', '') != 'icmp':
-            return True
-
     for rule in firewall.get('allowed'):
-        if rule.get('ports') == '0-65535' and rule.get('IPProtocol', '') != 'icmp':
+        if rule.get('IPProtocol', '') == 'icmp':
+            continue
+        if not rule.get('ports'):
+            return True
+        if rule.get('ports') == '0-65535':
             return True
 
     return False
 
 
 def firewall_id(firewall):
-    """
-    TODO
-    """
+    """A getter fn for test ids for Firewalls"""
     return "{} {}".format(firewall['id'], firewall['name'])
