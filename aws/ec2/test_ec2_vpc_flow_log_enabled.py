a9c11265d72ab56008a7718463f59f4a3f52d0bf
AJ Bahnken <aj@ajvb.me>
2018-04-04 09:22:59 -0700

    Move resource calls to fixtures

--- aws/ec2/test_ec2_vpc_flow_log_enabled.py
+++ aws/ec2/test_ec2_vpc_flow_log_enabled.py
@@ -1,17 +1,20 @@
 import pytest
 
-#from aws.ec2.helpers import ()
 from aws.ec2.resources import (
     ec2_flow_logs,
     ec2_vpcs
 )
 
 
+@pytest.fixture
+def all_flow_logs():
+    return ec2_flow_logs()
+
+
 @pytest.mark.ec2
 @pytest.mark.parametrize('ec2_vpc', ec2_vpcs(), ids=lambda vpc: vpc['VpcId'])
-def test_ec2_vpc_flow_log_enabled(ec2_vpc):
+def test_ec2_vpc_flow_log_enabled(ec2_vpc, all_flow_logs):
     """
     Checks that each VPC has VPC Flow Logs enabled.
     """
-    flow_logs = ec2_flow_logs()
-    assert any(flow_log for flow_log in flow_logs if flow_log['ResourceId'] == ec2_vpc['VpcId'])
+    assert any(flow_log for flow_log in all_flow_logs if flow_log['ResourceId'] == ec2_vpc['VpcId'])
