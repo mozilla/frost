45379c6260567e9bcac9674260caa4165f615913
Julien Vehent <julien@linuxwall.info>
2019-03-20 12:33:05 -0400

    Update required ami test logic

--- aws/ec2/test_ec2_instance_running_required_amis.py
+++ aws/ec2/test_ec2_instance_running_required_amis.py
@@ -1,25 +1,39 @@
 import pytest
 
 from aws.ec2.helpers import ec2_instance_test_id
-from aws.ec2.resources import ec2_instances
+from aws.ec2.resources import ec2_instances, ec2_images_owned_by
+
+from datetime import datetime, timedelta
 
 
 @pytest.fixture
-def required_amis(pytestconfig):
-    return frozenset(pytestconfig.custom_config.aws.required_amis)
+def owned_amis():
+    return ec2_images_owned_by()
 
 
 @pytest.mark.ec2
 @pytest.mark.parametrize("ec2_instance", ec2_instances(), ids=ec2_instance_test_id)
-def test_ec2_instance_running_required_amis(ec2_instance, required_amis):
+def test_ec2_instance_on_acceptable_ami(ec2_instance, owned_amis,):
     """
-    Checks that all EC2 instances are running the required AMIs.
+    Checks that all EC2 instances are running on acceptable AMIs, meaning
+    an AMI that is no only than 180 and is owned by us.
     """
-    if len(required_amis) == 0:
-        pytest.skip("No required AMIs were provided")
+    for tag in ec2_instance["Tags"]:
+        if tag["Key"] == "Name":
+            instanceName = tag["Value"]
 
-    assert (
-        ec2_instance["ImageId"] in required_amis
-    ), "EC2 Instance {0[InstanceId]} is running unexpected AMI: {0[ImageId]}".format(
-        ec2_instance
-    )
+    nightyDaysAgo = datetime.now() - timedelta(days=180)
+    foundAmi = False
+    for ami in owned_amis:
+        if ami["ImageId"] == ec2_instance["ImageId"]:
+            # assert ami age < 180 days
+            assert(ami["CreationDate"] > nightyDaysAgo), "Instance {} {} is running on an AMI created on {} that's older than 180 days".format(
+                instanceName, ec2_instance["InstanceId"], ami["CreationDate"]
+            )
+            foundAmi = True
+            break
+
+    if not foundAmi:
+        assert False, "Instance {} {} uses AMI {} not owned by us".format(
+            instanceName, ec2_instance["InstanceId"], ec2_instance["ImageId"]
+        )
