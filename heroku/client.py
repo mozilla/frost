1d9c3d16e8e5a423901519fafa881d3d875954e9
Hal Wine <hwine@mozilla.com>
2019-06-12 14:24:31 -0700

    Working heroku membership check
    Asserts on any mismatch between MFA status and email address format.

--- heroku/client.py
+++ heroku/client.py
@@ -1,5 +1,9 @@
-# from herokuadmintools import verify_access, session
-from herokuadmintools import find_users_missing_2fa, find_affected_apps
+import json
+
+from herokuadmintools import get_users
+
+# ToDo:
+#  - make cache work
 
 
 def cache_key(organization, method, *args, **kwargs):
@@ -13,23 +17,30 @@ def cache_key(organization, method, *args, **kwargs):
     ... )
     'pytest_heroku:organization_name:method_name:arg1,arg2:kwarg1=True.json'
     """
-    return ':'.join([
-        'pytest_heroku',
-        str(organization),
-        str(method),
-        ','.join(args),
-        ','.join('{}={}'.format(k, v) for (k, v) in kwargs.items()),
-    ]) + '.json'
+    return (
+        ":".join(
+            [
+                "pytest_heroku",
+                str(organization),
+                str(method),
+                ",".join(args),
+                ",".join("{}={}".format(k, v) for (k, v) in kwargs.items()),
+            ]
+        )
+        + ".json"
+    )
 
 
-def get_heroku_resource(organization,
-                        method_name,
-                        call_args,
-                        call_kwargs,
-                        cache=None,
-                        result_from_error=None,
-                        debug_calls=False,
-                        debug_cache=False):
+def get_heroku_resource(
+    organization,
+    method_name,
+    call_args,
+    call_kwargs,
+    cache=None,
+    result_from_error=None,
+    debug_calls=False,
+    debug_cache=False,
+):
     """
     Fetches and return final data
 
@@ -38,7 +49,7 @@ def get_heroku_resource(organization,
         - cache all apps of member
     """
     if debug_calls:
-        print('calling', method_name, 'on', organization)
+        print("calling", method_name, "on", organization)
 
     result = None
     if cache is not None:
@@ -46,22 +57,30 @@ def get_heroku_resource(organization,
         result = cache.get(ckey, None)
 
         if debug_cache and result is not None:
-            print('found cached value for', ckey)
+            print("found cached value for", ckey)
 
     if result is None:
         # convert from defaultdict with values as sets
-        users = {k: list(v) for k, v in
-                 find_users_missing_2fa(organization).items()}
-        apps = {k: list(v) for k, v in
-                find_affected_apps(users, organization).items()}
-        result = [{
-            HerokuDataSets.ROLE_USER: users,
-            HerokuDataSets.APP_USER: apps,
-        }]
+        # each user is a dict as described here:
+        #  https://devcenter.heroku.com/articles/platform-api-reference#team-member
+        # import pudb; pudb.set_trace()
+        users = list(get_users(organization))
+        ##role_users = {k: list(v) for k, v in
+        ##        find_users_missing_2fa(organization).items()}
+        ## App data collection takes forever -- don't get unless needed
+        ##apps = {k: list(v) for k, v in
+        ##        find_affected_apps(users, organization).items()}
+        result = [
+            {
+                # HerokuDataSets.ROLE_USER: users,
+                # HerokuDataSets.APP_USER: apps,
+                HerokuDataSets.USER: users
+            }
+        ]
 
         if cache is not None:
             if debug_cache:
-                print('setting cache value for', ckey)
+                print("setting cache value for", ckey)
 
             cache.set(ckey, result)
 
@@ -72,7 +91,8 @@ class HerokuDataSets:
     # We use an IntEnum so keys can be ordered, which is done deep in some
     # libraries when we use the enums as dict keys
     ROLE_USER = 1  # tuple of (role, user)
-    APP_USER = 2   # tuple of (app, user)
+    APP_USER = 2  # tuple of (app, user)
+    USER = 2  # list of dicts with JSON response fields
 
 
 class HerokuAdminClient:
@@ -80,12 +100,7 @@ class HerokuAdminClient:
     # ensure we have access from instance
     data_set_names = HerokuDataSets
 
-    def __init__(self,
-                 organization,
-                 cache,
-                 debug_calls,
-                 debug_cache,
-                 offline):
+    def __init__(self, organization, cache, debug_calls, debug_cache, offline):
         self.organization = organization
         self.cache = cache
 
@@ -95,25 +110,30 @@ class HerokuAdminClient:
 
         self.results = []
 
-    def get(self,
-            method_name,
-            call_args,
-            call_kwargs,
-            result_from_error=None,
-            do_not_cache=False):
+    def get(
+        self,
+        method_name,
+        call_args,
+        call_kwargs,
+        result_from_error=None,
+        do_not_cache=False,
+    ):
 
         if self.offline:
             self.results = []
         else:
-            self.results = list(get_heroku_resource(
-                self.organization,
-                method_name,
-                call_args,
-                call_kwargs,
-                cache=self.cache if not do_not_cache else None,
-                result_from_error=result_from_error,
-                debug_calls=self.debug_calls,
-                debug_cache=self.debug_cache))
+            self.results = list(
+                get_heroku_resource(
+                    self.organization,
+                    method_name,
+                    call_args,
+                    call_kwargs,
+                    cache=self.cache if not do_not_cache else None,
+                    result_from_error=result_from_error,
+                    debug_calls=self.debug_calls,
+                    debug_cache=self.debug_cache,
+                )
+            )
 
         return self
 
@@ -166,26 +186,6 @@ class HerokuAdminClient:
             tmp.append(keyed_result)
 
         self.results = tmp
-        return self
-
-    def flatten(self):
-        """
-        Flattens one level of a nested list:
-
-        >>> c = HerokuAdminClient([None], None, None, None, offline=True)
-        >>> c.results = [['A', 1], ['B']]
-        >>> c.flatten().values()
-        ['A', 1, 'B']
-
-        Only works for a list of lists:
-
-        >>> c.results = [{'A': 1}, {'B': 2}]
-        >>> c.flatten().values()
-        Traceback (most recent call last):
-        ...
-        TypeError: can only concatenate list (not "dict") to list
-        """
-        self.results = sum(self.results, [])
         return self
 
     def debug(self):
--- heroku/test_heroku_2fa.py
+++ heroku/test_heroku_2fa.py
@@ -7,59 +7,45 @@
 #       quite noisy. :/  We're trading off (for the moment at least) test
 #       readability for output readability.
 
-# code for heroku/conftest.py START
 import pytest
-from heroku.resources import users_no_2fa, app_users_no_2fa
+
+from conftest import heroku_client
 
 
-@pytest.fixture
-def affected_users():
-    roles = users_no_2fa()
-    # assert isinstance(roles, type([]))
-    role = roles[0]
-    # assert isinstance(role, type({}))
-    return role
+@pytest.fixture(scope="session")
+def raw_json_data():
+    """
+    """
+    raw = heroku_client.get("all", [], {})
+    return raw
 
 
-@pytest.fixture
-def affected_apps():
-    apps = app_users_no_2fa()
-    # assert isinstance(apps, type([]))
-    app = apps[0]
-    # assert isinstance(app, type({}))
-    return app
+@pytest.fixture(scope="session")
+def users(raw_json_data):
+    """
+    Get the members for this organization/team
+
+    Each dictionary is as described at
+        https://devcenter.heroku.com/articles/platform-api-reference#team-member
+
+    Returns: a list of member dictionaries
+    """
+    just_key = raw_json_data.extract_key(heroku_client.data_set_names.USER, [])
+    members = list(*just_key.results)
+
+    return members
 
 
-@pytest.fixture
-def role_user():
-    result = [(role, email) for role, emails in affected_users().items()
-              for email in emails]
-    # assert len(result) > 0
-    return result
-
-
-@pytest.fixture
-def app_user():
-    result = [(app, email) for app, emails in affected_apps().items()
-              for email in emails]
-    # assert len(result) > 0
-    return result
-
-
-# ##def pytest_generate_tests(metafunc):
-# ##    if 'role_user' in metafunc.fixturenames:
-# ##        metafunc.parametrize('role_user', role_user())
-# ##    elif 'app_user' in metafunc.fixturenames:
-# ##        metafunc.parametrize('app_user', app_user())
-
-# code for heroku/conftest.py END
-@pytest.mark.heroku
-@pytest.mark.parametrize('role_user', role_user())
-def test_users_have_2fa_enabled(role_user):
-    assert not role_user
-
-
-@pytest.mark.heroku
-@pytest.mark.parametrize('app_user', app_user())
-def test_no_apps_impacted(app_user):
-    assert not app_user
+@pytest.mark.parametrize("a_user", list(users(raw_json_data())))
+def test_mozilla_user(a_user):
+    # print(type(a_user), str(a_user))
+    email = a_user["email"]
+    if a_user["federated"]:
+        assert not email.startswith("heroku")
+        assert email.endswith("@mozilla.com")
+    elif a_user["two_factor_authentication"]:
+        # may have @m.c if starts with "heroku-"
+        if email.endswith("@mozilla.com"):
+            assert email.startswith("heroku")
+    else:
+        assert not email
