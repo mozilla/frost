cf0b6beb1155a3b9cc1f00a78a26bd2ee3527748
Hal Wine <hwine@mozilla.com>
2019-07-03 13:22:21 -0700

    now with warning for inconsistent metadata

--- github/helpers.py
+++ github/helpers.py
@@ -1,95 +1,19 @@
-import io
-import itertools
-import json
-import os
-import tempfile
+import functools
 
 import pyjq
-import pytest
+import resources
 
 
-
-
-
-
-##@pytest.fixture(scope="session")
-##def get_data_for_org(
-##    org_filler,
-##    date,
-##    method_name=None,
-##    call_args=None,
-##    call_kwargs=None,
-##    cache=None,
-##    result_from_error=None,
-##    debug_calls=False,
-##    debug_cache=False,
-##):
-##    srcname = f"{date}-{org_filler}.db.obj.json"
-##    print(f"date: {type(date)}, {date}")
-##    assert date
-##    data = get_data_from_file(srcname)
-##    return data
-
-#@pytest.fixture(scope="session")
-
-##@pytest.fixture(scope="session")
-##
-##def get_aux_json(aux_name):
-##    return parse_data_to_json(get_data_from_file(aux_files[aux_name]))
-##
-##@pytest.fixture(scope="module")
-##def get_org_json(get_data_for_org):
-##    return parse_data_to_json(get_data_for_org)
-##
-##@pytest.fixture(scope="module")
-##def repos_of_interest_for_org(org_filler):
-##    org = org_filler
-##    of_interest = get_aux_json("repos_of_interest")
-##    jq_script = f"""
-##        .[] | # for each array element
-##        .repo |  # only look at repository URL
-##        select(startswith("https://github.com/{org}")) # only in org
-##    """
-##    return pyjq.all(jq_script, of_interest)
-##
-##@pytest.fixture(scope="module")
-##        #params=list(repos_of_interest_for_org(org_filler)))
-##def org_default_branches(org_filler, get_org_json, date):
-##    """
-##    Return [(org, repo, default_branch)]
-##    """
-##    org_json = get_org_json
-##    urls = repos_of_interest_for_org(org_filler)
-##    print(f"org_json: {len(get_org_json)}; urls: {len(urls)}")
-##    results = []
-##    for url in urls:
-##        repo = url.split('/')[-1]
-##        assert repo.endswith(".git")
-##        repo = repo[:-4]
-##        repo_api_url = f"/repos/{org_filler}/{repo}"
-##        jq_for_repo = f"""
-##        . [] |  # for all elements
-##        select(.url == "{repo_api_url}")  # just this repo
-##        """
-##        record = pyjq.all(jq_for_repo, org_json)
-##        if len(record):
-##            branch = record[0]["body"]["default_branch"]
-##            assert branch
-##            results.append((org_filler, repo, branch, date))
-##        else:
-##            # noexistant === protected, but we want to update our
-##            # metadata
-##            print(f"couldn't find record for {repo}")
-##    return results
-##
-##@pytest.fixture(scope="module",
-##    params=org_default_branches(organization, get_org_json(get_data_for_org(organization, today)), today),
-##    ids=[f"{x[1]}-{x[2]}" for x in org_default_branches(organization, get_org_json(get_data_for_org(organization, today)), today)],
-##)
-##def def_branch(request):
-##    return request.param
-##
-##@pytest.fixture(scope="session", params=[today,])
-##def date(request):
-##    assert request.param, "TODAY not set in environment"
-##    return request.param
+@functools.lru_cache()
+def is_repo_archived(org, repo):
+    """
+    Return True if repo is archived
+    """
+    org_json = resources.get_org_json(org)
+    jq_script = f"""
+    .[] |  # for each element
+    select(.url == "/repos/{org}/{repo}") # just this repo
+    """
+    repo_data = pyjq.all(jq_script, org_json)
+    assert len(repo_data) == 1
+    return repo_data[0]["body"]["archived"]
--- github/test_branch_protection.py
+++ github/test_branch_protection.py
@@ -1,7 +1,8 @@
 """
-Check for branch protection
+Check for branch protection consistancy and compliance
 """
-import resources as r
+import helpers
+import resources
 import pyjq
 import pytest
 
@@ -17,7 +18,7 @@ be repos we care about in it. (Warning)
 )
 @pytest.mark.parametrize(
         "repo_list",
-        [(r.repos_of_interest_for_org()),],
+        [(resources.repos_of_interest_for_org()),],
 )
 def test_repos(repo_list):
     assert len(repo_list) > 0
@@ -35,7 +36,7 @@ degenerate.) (Warning)
 )
 @pytest.mark.parametrize(
         "repo_default_branches",
-        [(r.org_default_branches()),],
+        [(resources.org_default_branches()),],
 )
 def test_org_branches(repo_default_branches):
     assert len(repo_default_branches) > 0
@@ -49,10 +50,14 @@ Compliance check: production branches must be protected.
 )
 @pytest.mark.parametrize(
         "org, repo, branch, date",
-        r.org_default_branches(),
+        resources.org_default_branches(),
 )
 def test_default_branch_protected(org, repo, branch, date):
-    org_json = r.get_org_json(org)
+    org_json = resources.get_org_json(org)
+    # by definition, archived repos are protected. This means meta data
+    # is out of sync, but that's tested elsewhere
+    if helpers.is_repo_archived(org, repo):
+        return
     jq_branch_record = f"""
         .[] |  # for each element
         select(.url == "/repos/{org}/{repo}/branches/{branch}") # just this branch
@@ -60,3 +65,19 @@ def test_default_branch_protected(org, repo, branch, date):
     branch_data = pyjq.all(jq_branch_record, org_json)
     assert len(branch_data) == 1
     assert branch_data[0]["body"]["protected"]
+
+
+
+@pytest.mark.github_org
+@pytest.mark.xfail(reason="Archived repo in metadata active list")
+@pytest.mark.rationale(
+        """
+Consistency check: branches in metadata should not be archived
+"""
+)
+@pytest.mark.parametrize(
+        "org, repo, branch, date",
+        resources.org_default_branches(),
+)
+def test_no_archived_repos_in_active_metadata(org, repo, branch, date):
+    assert helpers.is_repo_archived(org, repo)
