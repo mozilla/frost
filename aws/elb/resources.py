0ed350ec4966cae3346681a8ecfd0683f7946c74
AJ Bahnken <1144310+ajvb@users.noreply.github.com>
2020-08-05 11:42:29 -0700

    Get associated tags for elbs (#320)

--- aws/elb/resources.py
+++ aws/elb/resources.py
@@ -5,12 +5,35 @@ def elbs():
     """
     http://botocore.readthedocs.io/en/latest/reference/services/elb.html#ElasticLoadBalancing.Client.describe_load_balancers
     """
-    return (
+    elbs = (
         botocore_client.get("elb", "describe_load_balancers", [], {})
         .extract_key("LoadBalancerDescriptions")
         .flatten()
         .values()
     )
+
+    elbs_with_tags = []
+    for elb in elbs:
+        tags = (
+            botocore_client.get(
+                service_name="elb",
+                method_name="describe_tags",
+                call_args=[],
+                call_kwargs={"LoadBalancerNames": [elb["LoadBalancerName"]]},
+                regions=[elb["__pytest_meta"]["region"]],
+            )
+            .extract_key("TagDescriptions")
+            .flatten()
+            .values()
+        )
+        # This check is probably unneeded
+        if len(tags) >= 1:
+            tags = tags[0]
+        if "Tags" in tags:
+            elb["Tags"] = tags["Tags"]
+        elbs_with_tags.append(elb)
+
+    return elbs_with_tags
 
 
 def elbs_v2():
