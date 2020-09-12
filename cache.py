c1c14c24c060dec95fbd6700f00dded6bdccd7d1
Brian Pitts <bpitts@mozilla.com>
2020-09-11 21:07:44 -0400

    Paint it black

--- cache.py
+++ cache.py
@@ -36,7 +36,7 @@ def json_iso_datetime_string_to_datetime(obj):
 
 
 def datetime_encode_set(self, key, value):
-    """ save value for the given key.
+    """save value for the given key.
 
     :param key: must be a ``/`` separated value. Usually the first
          name is the name of your plugin or your application.
@@ -61,7 +61,7 @@ def datetime_encode_set(self, key, value):
 
 
 def datetime_encode_get(self, key, default):
-    """ return cached value for the given key.  If no value
+    """return cached value for the given key.  If no value
     was yet cached or the value cannot be read, the specified
     default is returned.
 
