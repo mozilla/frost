1d00e7f23e716b4c555bacaefea4419331976d56
AJ Bahnken <aj@ajvb.me>
2020-09-15 11:31:34 -0700

    switch ordering on rds id func for unit tests

--- aws/rds/test_rds_db_instance_not_publicly_accessible_by_vpc_sg.py
+++ aws/rds/test_rds_db_instance_not_publicly_accessible_by_vpc_sg.py
@@ -15,7 +15,7 @@ from aws.rds.helpers import (
     ["rds_db_instance", "ec2_security_groups"],
     zip(rds_db_instances_with_tags(), rds_db_instances_vpc_security_groups()),
     ids=lambda db: get_db_instance_id(db)
-    if "DBInstanceIdentifier" in db and isinstance(db, dict)
+    if isinstance(db, dict) and "DBInstanceIdentifier" in db
     else None,
 )
 def test_rds_db_instance_not_publicly_accessible_by_vpc_security_group(
