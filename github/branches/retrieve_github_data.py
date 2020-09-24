mozilla/frost

commit dcac1cd50ad8c0c1f7b8fe927e17b0725b4428ff

Add v3 & v4 ids for branch data
the v4 & v3 id's are immutable, names are not

github/branches/retrieve_github_data.py
@@ -43,12 +43,10 @@ logger = logging.getLogger(__name__)
 class BranchName:
     name: str
     prefix: str
-    repo_id: str = None  # only available in the top level query
-    owner_id: str = None
 
     @classmethod
     def csv_header(cls) -> List[str]:
-        return ["Branch Name", "Ref Prefix", "Repo_id", "Owner_id"]
+        return ["Branch Name", "Ref Prefix"]
 
     @classmethod
     def cvs_null(cls) -> List[str]:
@@ -58,13 +56,13 @@ class BranchName:
         return [
             self.name or None,
             self.prefix or None,
-            self.repo_id or None,
-            self.owner_id or None,
         ]
 
 
 @dataclass
 class BranchProtectionRule:
+    bpr_v4id: str
+    bpr_v3id: str
     is_admin_enforced: bool
     push_actor_count: int
     rule_conflict_count: int
@@ -74,6 +72,8 @@ class BranchProtectionRule:
     @classmethod
     def csv_header(cls) -> List[str]:
         return [
+            "bpr_v4id",
+            "bpr_v3id",
             "Inc Admin",
             "Restricted Pusher Count",
             "Conflicting Rule Count",
@@ -82,10 +82,12 @@ class BranchProtectionRule:
 
     @classmethod
     def cvs_null(cls) -> List[str]:
-        return [None, None, None, None] + BranchName.cvs_null()
+        return [None, None, None, None, None, None] + BranchName.cvs_null()
 
     def csv_row(self) -> List[str]:
         my_info = [
+            self.bpr_v4id,
+            self.bpr_v3id,
             self.is_admin_enforced,
             self.push_actor_count,
             self.rule_conflict_count,
@@ -103,14 +105,27 @@ class BranchProtectionRule:
 class RepoBranchProtections:
     default_branch_ref: str
     name_with_owner: str
+    owner_v4id: str
+    repo_v4id: str
+    repo_v3id: str
     protection_rules: List[BranchProtectionRule] = field(default_factory=list)
 
     @classmethod
     def csv_header(cls) -> List[str]:
-        return ["Login", "Repo"] + BranchProtectionRule.csv_header()
+        return [
+            "Login",
+            "Repo",
+            "repo_v4id",
+            "repo_v3id",
+            "default branch",
+            "owner_v4id",
+        ] + BranchProtectionRule.csv_header()
 
     def csv_row(self) -> List[str]:
         my_info = list(self.name_with_owner.split("/"))
+        my_info.extend(
+            (self.repo_v4id, self.repo_v3id, self.default_branch_ref, self.owner_v4id)
+        )
         result = []
         for rule in self.protection_rules:
             for line in rule.csv_row():
@@ -125,7 +140,7 @@ def _add_protection_fields(node) -> None:
 
     In normal gQuery, this would be a fragment
     """
-    node.__fields__(is_admin_enforced=True, id=True, pattern=True)
+    node.__fields__(is_admin_enforced=True, id=True, pattern=True, database_id=True)
     node.branch_protection_rule_conflicts(first=0).__fields__(total_count=True,)
     node.push_allowances(first=0).__fields__(total_count=True,)
 
@@ -148,6 +163,7 @@ def create_operation(owner, name):
     repo.default_branch_ref.__fields__(name=True)
     repo.name_with_owner()
     repo.id()
+    repo.database_id()
     repo.owner().id()
     repo.owner().login()
 
@@ -172,7 +188,7 @@ def create_operation(owner, name):
     ref.total_count()
     ref.page_info.__fields__(end_cursor=True, has_next_page=True)
     ref.nodes().__fields__(
-        name=True, prefix=True, id=True,
+        name=True, prefix=True,
     )
 
     return op
@@ -278,12 +294,17 @@ def extract_branch_data(repodata) -> RepoBranchProtections:
     """
     repo_data = RepoBranchProtections(
         name_with_owner=repodata.name_with_owner,
-        default_branch_ref=repodata.default_branch_ref,
+        default_branch_ref=repodata.default_branch_ref.name,
+        repo_v4id=repodata.id,
+        repo_v3id=repodata.database_id,
+        owner_v4id=repodata.owner.id,
     )
     # Add in each rule for this repo
     rules = []
     for r in repodata.branch_protection_rules.nodes:
         rule = BranchProtectionRule(
+            r.id,
+            r.database_id,
             r.is_admin_enforced,
             r.push_allowances.total_count,
             r.branch_protection_rule_conflicts.total_count,

github/orgs/retrieve_github_data.py
@@ -47,7 +47,7 @@ class OrgInfo:
 
     @classmethod
     def csv_header(cls) -> List[str]:
-        return ["Org Name", "Org Slug", "2FA Required", "id", "db id"]
+        return ["Org Name", "Org Slug", "2FA Required", "v4id", "v3id"]
 
     @classmethod
     def cvs_null(cls) -> List[Optional[str]]:

github/vscode-debug-wrapper.py
@@ -19,7 +19,7 @@ from github.orgs import retrieve_github_data as org_retrieve_github_data
 from github.branches import retrieve_github_data as branch_retrieve_github_data
 
 # org will get metadata orgs if none supplied
-org_retrieve_github_data.main()
+# org_retrieve_github_data.main()
 
 # branch does not have default, so pass along current command line
 # N.B. since that will also happen in pytest's doctest mode, that

commit 79947675e75c50f80a9214bc2f8af1c71015076d

Fix "no attribute" Exception
Commit dcac1cd broke this by performing a dereference at an earlier
stage, and not removing the (now incorrect) later dereference.

github/branches/retrieve_github_data.py
@@ -17,7 +17,6 @@ from typing import Any, List
 from sgqlc.operation import Operation  # noqa: I900
 from sgqlc.endpoint.http import HTTPEndpoint  # noqa: I900
 
-# from branch_check.github_schema import github_schema as schema  # noqa: I900
 from github import github_schema as schema  # noqa: I900
 
 DEFAULT_GRAPHQL_ENDPOINT = "https://api.github.com/graphql"

github/branches/validate_compliance.py
@@ -79,7 +79,7 @@ def validate_branch_protections(
 
     # if the production branch is not specified, use the default name
     if not branch:
-        branch = data.default_branch_ref.name
+        branch = data.default_branch_ref
 
     # Multiple steps to validate - first, is the branch even covered
     active_rules = find_applicable_rules(data, branch)