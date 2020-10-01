6a1f0fc6ac47f3ca31bdd791e49552a2b95f14d8
Hal Wine <hwine@mozilla.com>
2020-10-01 11:24:21 -0700

    Heroku breaking build, remove for now
    This was never fully finished, and is not used in production.

--- heroku/conftest.py
+++
@@ -1,8 +0,0 @@
-def pytest_configure(config):
-    # register custom marks for heroku services
-    for svc_name in [
-        "heroku",
-    ]:
-        config.addinivalue_line(
-            "markers", "{}: mark tests against {}".format(svc_name, svc_name)
-        )
