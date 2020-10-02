322121acc794daad48b142b479128a2361779537
Robert Micheletto <bobm@mozilla.com>
2020-10-02 15:52:10 -0400

    Adds topic test, and works except for the lambda function.

--- aws/sns/resources.py
+++ aws/sns/resources.py
@@ -10,6 +10,14 @@ def sns_subscriptions():
         .values()
     )
 
+def sns_topics():
+    "https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sns.html#topic"
+    return (
+        botocore_client.get("sns", "list_topics", [], {})
+        .extract_key("Topics")
+        .flatten()
+        .values()
+    )
 
 def sns_subscription_attributes():
     "https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sns.html#subscription"
@@ -25,4 +33,20 @@ def sns_subscription_attributes():
         .extract_key("Attributes")
         .values()[0]
         for subscription in sns_subscriptions()
+    ]
+
+def sns_subscriptions_by_topic():
+    "https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sns.html#topic"
+    return [
+        botocore_client.get(
+            service_name="sns",
+            method_name="list_subscriptions_by_topic",
+            call_args=[],
+            call_kwargs={"TopicArn": topic["TopicArn"]},
+            profiles=[topic["__pytest_meta"]["profile"]],
+            regions=[topic["__pytest_meta"]["region"]],
+        )
+        .extract_key("Subscriptions")
+        .values()[0]
+        for topic in sns_topics()
     ]
