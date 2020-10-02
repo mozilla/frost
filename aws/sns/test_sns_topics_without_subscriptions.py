5dd7e379e383c20a7ad1eafbceb98a5d1e633c10
Robert Micheletto <bobm@mozilla.com>
2020-10-02 16:47:51 -0400

    Adds rationale.

--- aws/sns/test_sns_topics_without_subscriptions.py
+++ aws/sns/test_sns_topics_without_subscriptions.py
@@ -4,6 +4,12 @@ from helpers import get_param_id
 from aws.sns.resources import sns_subscriptions_by_topic
 
 @pytest.mark.sns
+@pytest.mark.rationale(
+    """
+SNS Topics without subscriptions have no place to deliver messages.
+They are good candidates for removal, or appropriate subscriptions.
+"""
+)
 @pytest.mark.parametrize(
     "topic",
     sns_subscriptions_by_topic(),
