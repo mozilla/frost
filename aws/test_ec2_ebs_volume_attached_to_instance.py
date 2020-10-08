275ba96debe16e93e278d07b28d493f0515b55bf
bqbn <bqbn@openken.com>
2020-10-07 17:48:01 -0700

    Add a doc string

--- aws/test_ec2_ebs_volume_attached_to_instance.py
+++ aws/test_ec2_ebs_volume_attached_to_instance.py
@@ -11,6 +11,11 @@ from aws.ec2.helpers import ebs_volume_attached_to_instance
     "ec2_ebs_volume", ec2_ebs_volumes(), ids=lambda ebs: get_param_id(ebs, "VolumeId")
 )
 def test_ec2_ebs_volume_attached_to_instance(ec2_ebs_volume):
+    """Normally cloudops would like to have all EBS volumes attached to
+    instances once they are created, for the purpose of cost saving, thus we
+    have this check. There is a default 90 day grace period though for the ebs
+    volumes, e.g. a volume may not be attached to any instance for 90 days.
+    """
     assert ebs_volume_attached_to_instance(
         ec2_ebs_volume
     ), f"{ec2_ebs_volume['VolumeId']} is created at {ec2_ebs_volume['CreateTime']}, and is not attached to an instance."
