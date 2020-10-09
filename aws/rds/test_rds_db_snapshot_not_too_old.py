945e7dc6cca8ff8a91562dbfe5b0649ea0826bac
bqbn <bqbn@openken.com>
2020-10-09 14:51:44 -0700

    Make black happy

--- aws/rds/test_rds_db_snapshot_not_too_old.py
+++ aws/rds/test_rds_db_snapshot_not_too_old.py
@@ -9,5 +9,6 @@ from aws.rds.helpers import get_db_snapshot_arn, rds_db_snapshot_not_too_old
     "rds_db_snapshot", rds_db_snapshots(), ids=get_db_snapshot_arn,
 )
 def test_rds_db_snapshot_not_too_old(rds_db_snapshot):
-    assert rds_db_snapshot_not_too_old(rds_db_snapshot
-        ), f"{rds_db_snapshot['DBSnapshotIdentifier']} is created at {rds_db_snapshot['SnapshotCreateTime']}, and is considered too old."
+    assert rds_db_snapshot_not_too_old(
+        rds_db_snapshot
+    ), f"{rds_db_snapshot['DBSnapshotIdentifier']} is created at {rds_db_snapshot['SnapshotCreateTime']}, and is considered too old."
