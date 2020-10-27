ed12df32a8e03fbc4b6bc92362d93eebf467eb9e
AJ Bahnken <1144310+ajvb@users.noreply.github.com>
2020-10-27 10:08:54 -0700

    Fix exemptions (#403)
    Have exemptions work as expected by properly parsing out the "test id"
    (defined by the parameterize's `id` func)

--- conftest.py
+++ conftest.py
@@ -153,7 +153,9 @@ def gcp_config(pytestconfig):
 
 
 def pytest_runtest_setup(item):
-    """"""
+    """
+    Add custom markers to pytest tests.
+    """
     if not isinstance(item, DoctestItem):
         item.config.custom_config.add_markers(item)
 
